75cbd83e6ddd1f51d7c810593dc72f4edc3dce80
James Harding <jamesvitaly1993@gmail.com>
2022-12-16 16:59:40 +0000

    Added more error handling

--- hooks/usePlayer.js
+++ hooks/usePlayer.js
@@ -41,6 +41,7 @@ const usePlayer = ({ playlist }) => {
   // }
 
   function error() {
+    console.log('error', audio.error)
     if(audio?.error && audio?.error?.message === ''){
       setError('This file format cannot be played on your browser');
     }
